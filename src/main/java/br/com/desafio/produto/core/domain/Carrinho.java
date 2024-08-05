7f493d4031c713fe8af96c1883486dad760f3e23
jhonnyscerni <jhonnyscerni@gmail.com>
2024-08-05 12:30:52 -0300

    #Desafio - ajustes

--- src/main/java/br/com/desafio/produto/core/domain/Carrinho.java
+++ src/main/java/br/com/desafio/produto/core/domain/Carrinho.java
@@ -9,7 +9,6 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class Carrinho {
 
--- src/main/java/br/com/desafio/produto/core/domain/Pedido.java
+++ src/main/java/br/com/desafio/produto/core/domain/Pedido.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class Pedido {
 
